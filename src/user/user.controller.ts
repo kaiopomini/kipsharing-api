488dce80ad007e010b6a1737c3a7d3ac53fc6bc4
Kaio Pomini <kaiopomini@gmail.com>
2023-01-08 19:30:03 -0300

    chore(rolesEnum)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,7 +8,6 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
-  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto, UpdateUserDto } from './dto';
